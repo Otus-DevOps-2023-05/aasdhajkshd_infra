21a1f6529efc807adc7bba89da74ca3bd505c017
AAsdhajkshD <elnone@gmail.com>
2023-08-11 13:51:49 +0300

    попытка не пытка 16

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -1,6 +1,4 @@
 terraform {
-  required_version = ">= 1.0.0"
-
   backend "s3" {
     endpoint   = "storage.yandexcloud.net"
     bucket     = "b1gk5pck1n1mtgfoijib"
--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -1,5 +1,4 @@
 terraform {
-  required_version = ">= 1.0.0"
 
   backend "s3" {
     endpoint   = "storage.yandexcloud.net"
