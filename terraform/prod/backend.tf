8d11bd0001e7fed3c4e974e4798e46e4dc8970f4
AAsdhajkshD <elnone@gmail.com>
2023-08-11 07:36:42 +0300

    ansible-1, вторая попытка

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -6,8 +6,6 @@ terraform {
     bucket     = "b1gk5pck1n1mtgfoijib"
     region     = "ru-central1"
     key        = "terraform.tfstate"
-    access_key = "YCAJEORbqMpUghLYTe_k_3k_j"
-    secret_key = "YCMldBRw8ISfi_OZcEWyoQjTr7LZnY-8flQcWPgP"
     skip_region_validation      = true
     skip_credentials_validation = true
   }
